904955817528a34dedee1b05a9d1b6245d42bff9
SethupathyD <sethupathy.d@syncfusion.com>
2025-02-17 14:05:02 +0530

    Code updated.

---
+++ SfDataGridSample/SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.Maui.DataGrid;
+
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void sfGrid_CellTapped(object sender, DataGridCellTappedEventArgs e)
+        {
+            DisplayAlert("Cell Tapped", $"Cell tap performed on row {e.RowColumnIndex.RowIndex} and column {e.RowColumnIndex.ColumnIndex}.", "OK");
+        }
+
+        private void sfGrid_CellDoubleTapped(object sender, DataGridCellDoubleTappedEventArgs e)
+        {
+            DisplayAlert("Cell Double Tapped", $"Cell Double tap performed on row {e.RowColumnIndex.RowIndex} and column {e.RowColumnIndex.ColumnIndex}.", "OK");
+        }
+    }
+}
